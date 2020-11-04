9c621aa7acb50faf9122742a8a0fe3672b0c9542
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-11-03 23:36:35 -0800

    add example with msl times

--- example/index.js
+++ example/index.js
@@ -1,20 +1,47 @@
 import * as Timecraft from '../src/index.js';
 
-Promise.all([
-    '../kernels/lsk/naif0012.tls',
-    '../kernels/pck/pck00010.tpc',
-    '../kernels/spk/de425s.bsp',
-].map( p => fetch( p ).then( res => res.arrayBuffer() ) ) )
-    .then( buffers => {
+( async function() {
 
-        window.Timecraft = Timecraft;
+    const buffers = await Promise.all([
+        '../kernels/lsk/naif0012.tls',
+        '../kernels/pck/pck00010.tpc',
+        '../kernels/spk/de425s.bsp',
+        '../kernels/msl_v08.tf',
+        '../kernels/msl_lmst_gc120806_v2.tsc',
+    ].map( p => fetch( p ).then( res => res.arrayBuffer() ) ) );
 
-        buffers.forEach( buffer => {
+    buffers.forEach( buffer => {
 
-            Timecraft.loadKernelFromBuffer( buffer );
-        
-        } );
-        // console.log( buffers );
-
+        Timecraft.loadKernelFromBuffer( buffer );
 
     } );
+
+    window.Timecraft = Timecraft;
+    const utcEl = document.querySelector('[name="utc"]');
+    const etEl = document.querySelector('[name="et"]');
+    const lstEl = document.querySelector('[name="mars lst"]');
+    const lmstEl = document.querySelector('[name="msl lmst"]');
+    const sclkEl = document.querySelector('[name="msl sclk"]');
+
+    setInterval(() => {
+
+        let utc = new Date().toISOString();
+        utc = utc.slice(0, utc.length - 1);
+
+        const et = Timecraft.Spice.utc2et(utc);
+
+        const lst = Timecraft.Spice.et2lst(et, 499, 0, 'planetocentric');
+
+        const lmst = Timecraft.Spice.sce2s(-76900, et);
+
+        const sclk = Timecraft.Spice.sce2c(-76900, et);
+
+        utcEl.childNodes[0].textContent = utc;
+        etEl.childNodes[0].textContent = et;
+        lstEl.childNodes[0].textContent = lst.time;
+        lmstEl.childNodes[0].textContent = lmst;
+        sclkEl.childNodes[0].textContent = sclk;
+
+    }, 100);
+
+} )();
