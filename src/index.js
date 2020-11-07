a6dcdf203b8c41fc20ca72eeb96c730c86b0428f
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-11-07 12:48:45 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import CSpice from './cspice.js';
 import * as Spice from './spice.js';
 import {
-    prepareFileFromBuffer,
-    removeFile,
     loadKernel,
-    loadKernelFromBuffer,
     unloadKernel,
     chronos,
     parseMetakernel,
