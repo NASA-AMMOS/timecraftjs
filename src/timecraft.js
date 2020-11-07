40a02534b4a8a36fcd1ee7cf8539adc4e680797f
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-11-07 11:19:09 -0800

    Update timecraft.js

--- src/timecraft.js
+++ src/timecraft.js
@@ -1,68 +1,40 @@
 import Module from './cspice.js';
 import * as Spice from './spice.js';
 
-let bufferFileCount = 0;
-
 const FS = Module.get_fs();
 
-function mkdirRecursive(parts) {
-    const tempParts = parts.slice();
-
-    let currPath = '';
-    while (tempParts.length) {
-        if (currPath !== '') {
-            currPath += '/';
-        }
-        currPath += tempParts.shift();
-        try {
-            FS.mkdir(currPath);
-        } catch (e) {
-            // do nothing
-        }
-    }
-}
-
-// file system manipulation
-// https://emscripten.org/docs/api_reference/Filesystem-API.html
-export function prepareFileFromBuffer(path, buffer) {
-    if (buffer instanceof ArrayBuffer) {
-        buffer = new Uint8Array(buffer);
-    }
-
-    const parts = path.split(/[\\/]/g);
-    parts.pop();
-    mkdirRecursive(parts);
-
-    FS.writeFile(path, buffer, { encoding: 'binary' });
-}
-
-export function removeFile(path) {
-    FS.unlink(path);
-}
+const fileMap = {};
+let bufferFileCount = 0;
 
 // loading kernels
-export function loadKernel(path) {
-    Spice.furnsh(path);
-}
-
-export function loadKernelFromBuffer(buffer) {
-    const path = `__buffer_files__/buffer_${ bufferFileCount }.bin`;
+export function loadKernel(buffer, key = null) {
+    if (key !== null && key in fileMap) {
+        throw new Error();
+    }
+    
+    const path = `_buffer_${ bufferFileCount }.bin`;
     bufferFileCount++;
+    fileMap[key] = path;
 
-    prepareFileFromBuffer(path, buffer);
-    loadKernel(path);
+    FS.writeFile(path, buffer, { encoding: 'binary' });
+    Spice.furnsh(path);
+    FS.unlink(path);
 }
 
 // unloading kernel
 export function unloadKernel(key) {
-    Spice.unload(key);
+    if (!(key in fileMap)) {
+        throw new Error();
+    }
+
+    Spice.unload(fileMap[key]);
+    delete fileMap[key];
 }
 
 // Chronos CLI
 // https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/ug/chronos.html
 
 export function chronos(inptim, cmdlin) {
-
     const outtim_ptr = Module._malloc(256);
     const intptr = Module._malloc(8);
 
@@ -78,7 +50,6 @@ export function chronos(inptim, cmdlin) {
     Module._free(outtim_ptr);
 
     return ret;
-
 }
 
 function processTokenValue(value) {
