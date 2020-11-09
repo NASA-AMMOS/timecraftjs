5435930ed5bc010be6b825c5d5ebd428109aead1
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-11-09 15:57:12 -0800

    Add cjs build

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import path from 'path';
+
+export default {
+    input: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        dir: path.resolve(__dirname, 'cjs'),
+        format: 'commonjs',
+        preserveModules: true,
+        strict: false,
+    },
+};
