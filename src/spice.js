93b78611a04c3c6b2d462c5dbc796832cc43dd11
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-11-11 09:57:03 -0800

    Fix error reporting functions, add tests

--- src/spice.js
+++ src/spice.js
@@ -316,12 +316,19 @@ export function deltet(epoch, eptype) {
  * @todo Document and test this!
  */
 export function erract(op, action) {
-    return Module.ccall(
+    const action_ptr = Module._malloc(100);
+    Module.stringToUTF8(action, action_ptr, 100);
+
+    Module.ccall(
         'erract_c',
         null,
-        ['string', 'number', 'string'],
-        [op, 100, action],
+        ['string', 'number', 'number'],
+        [op, 100, action_ptr],
     );
+
+    const result = _UTF8ToString(action_ptr, 100);
+    Module._free(action_ptr);
+    return result;
 }
 
 export function errprt(op, list) {
@@ -336,7 +343,7 @@ export function errprt(op, list) {
         [op, 100, list_ptr],
     );
 
-    const result = Module.UTF8ToString(list_ptr, 100);
+    const result = _UTF8ToString(list_ptr, 100);
     Module._free(list_ptr);
     return result;
 }
